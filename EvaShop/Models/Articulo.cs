2d96b8be520fe223de308f4c26a82a204d4dcef9
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-16 12:12:47 -0600

    Se a comenzado agregar las entidades bases.

---
+++ EvaShop/Models/Articulo.cs
@@ -0,0 +1,12 @@
+﻿namespace EvaShop.Models
+{
+    public class Articulo
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagenes { get; set; }
+        public string CodigoDeBarra { get; set; }
+        public Categoria Categoria  { get; set; }
+    }
+}
---
+++ EvaShop/Models/Inventario.cs
@@ -0,0 +1,13 @@
+﻿namespace EvaShop.Models
+{
+    public class Inventario
+    {
+        public int Id { get; set; }
+        public int ArticuloId { get; set; }
+        public Articulo Articulo { get; set; }
+        public int Existencias { get; set; }
+        public decimal Costo { get; set; }
+        public decimal PrecioDeVenta { get; set; }
+        public int CantidadMinima { get; set; }
+    }
+}
