d42b430fec50ee51fc0cf830189026be64303e16
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-19 11:22:09 -0600

    Se ha agregado estados en DataSeeder

--- EvaShop/Data/DataSeeder.cs
+++ EvaShop/Data/DataSeeder.cs
@@ -1,7 +1,15 @@
-﻿namespace EvaShop.Data
-{
-    public class DataSeeder
-    {
+﻿using EvaShop.Models;
 
+namespace EvaShop.Data
+{
+    public static class DataSeeder
+    {
+        public static readonly List<Estado> Estados = Enum.GetValues(typeof(EstadosIds))
+        .Cast<EstadosIds>()
+        .Select(e => new Estado
+        {
+            Id = e,
+            Nombre = e.ToString()
+        }).ToList();
     }
 }
