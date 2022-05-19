527b7b2d3f313475778d196ee4f90d9c266303d3
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-19 16:32:37 -0600

    Trabajando en el diseño...

---
+++ EvaShop/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EvaShop.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
