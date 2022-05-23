e49ff37277a515e822ff8a22ad7fe92619c409e2
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-22 22:02:53 -0600

    Implementando Product page (detalle de articulo).

--- EvaShop/Controllers/ProductController.cs
+++ EvaShop/Controllers/ProductController.cs
@@ -1,12 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using EvaShop.Data;
+using EvaShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EvaShop.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public ProductController(ApplicationDbContext appDbContext, IMapper mapper)
         {
-            return View();
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
+        public IActionResult Index(string id)
+        {
+            var articulo = _appDbContext.Inventarios
+                .Include(i=>i.Articulo)
+                .ThenInclude(i=>i.SubCategoria)
+                .ThenInclude(i=>i.Categoria)
+                .FirstOrDefault(i=> i.ArticuloId.ToString() == id);
+
+            return View(_mapper.Map<InventarioViewModel>(articulo));
         }
     }
 }
