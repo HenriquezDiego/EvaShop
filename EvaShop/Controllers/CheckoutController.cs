131ea0919387a6510dbb742566c9dc1ff2ae8d17
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-23 17:17:09 -0600

    Se ha iniciado la integración del carrito de compras con el checkout.

--- EvaShop/Controllers/CheckoutController.cs
+++ EvaShop/Controllers/CheckoutController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using EvaShop.Data;
+using EvaShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EvaShop.Controllers
 {
     public class CheckoutController : Controller
     {
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly IMapper _mapper;
+
+        public CheckoutController(ApplicationDbContext appDbContext, IMapper mapper)
+        {
+            _appDbContext = appDbContext;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
-            return View();
+            var bag = HttpContext.Session.GetString("_Bag");
+            if (string.IsNullOrEmpty(bag)) return Redirect("Home");
+            var myShopingCart = bag.Split(',').ToList();
+            var group = myShopingCart.GroupBy(x => x).Select((x) => new
+            {
+                x.Key,
+                x.ToList().Count
+            }).ToList();
+            var articulos = _appDbContext.Inventarios
+                .Include(i=>i.Articulo)
+                .ThenInclude(i=>i.SubCategoria)
+                .ThenInclude(i=>i.Categoria)
+                .ToList();
+            
+
+            articulos = articulos.Where(i => myShopingCart.Any(m => m == i.ArticuloId.ToString())).ToList();
+            var result = _mapper.Map<IEnumerable<ShopingCartViewModel>>(articulos);
+            foreach (var item in result)
+            {
+                item.Cantidad = group.FirstOrDefault(g => g.Key == item.ArticuloId.ToString())?.Count ?? 0;
+            }
+            return View(result);
         }
     }
 }
