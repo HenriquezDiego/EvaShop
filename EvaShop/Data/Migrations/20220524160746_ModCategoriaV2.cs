0131f96d72de273c2fbbb41103777793dbe33c94
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-24 10:09:00 -0600

    Se ha lanzado nueva migración para agregar propiedad Imagen en la entidad Categoría.

---
+++ EvaShop/Data/Migrations/20220524160746_ModCategoriaV2.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EvaShop.Data.Migrations
+{
+    public partial class ModCategoriaV2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Imagen",
+                table: "Categorias",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Imagen",
+                table: "Categorias");
+        }
+    }
+}
--- EvaShop/Models/Categoria.cs
+++ EvaShop/Models/Categoria.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
+        public string? Imagen { get; set; }
         public List<SubCategoria> SubCategorias { get; set; }
     }
 }
