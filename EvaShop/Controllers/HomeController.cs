c6723783b35d4ee79942db8fe9cc1e1d10d6e66a
Diego Henriquez <df.henriquez@hotmail.com>
2022-05-24 10:06:23 -0600

    Mejoras visuales.

--- EvaShop/Controllers/HomeController.cs
+++ EvaShop/Controllers/HomeController.cs
@@ -1,21 +1,39 @@
 ﻿using EvaShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using AutoMapper;
+using EvaShop.Data;
+using EvaShop.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace EvaShop.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _appDbContext;
+        private readonly IMapper _mapper;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+            ApplicationDbContext appDbContext,IMapper mapper)
         {
             _logger = logger;
+            _appDbContext = appDbContext;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var inventarios = _appDbContext.Inventarios
+                .Include(i=>i.Articulo)
+                .ThenInclude(i=>i.SubCategoria)
+                .ThenInclude(i=>i.Categoria)
+                .Where(i=>i.Existencias > 0)
+                .OrderBy(i=>i.PrecioDeVenta)
+                .Take(3)
+                .ToList();
+            var result =  _mapper.Map<IEnumerable<InventarioViewModel>>(inventarios);
+            return View(result);
         }
 
         public IActionResult Privacy()
