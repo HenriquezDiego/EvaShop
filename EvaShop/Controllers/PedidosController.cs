6f935e0b2b535ad27bbea122b3e2327e1f88bb19
Diego Henriquez <df.henriquez@hotmail.com>
2022-06-02 09:01:03 -0600

    Fix en _LayoutAdmin, y se ha agregado CRUD para los Pedidos.

---
+++ EvaShop/Controllers/PedidosController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EvaShop.Data;
+using EvaShop.Models;
+
+namespace EvaShop.Controllers
+{
+    public class PedidosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PedidosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pedidos
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pedidos.Include(p => p.Cliente).Include(p => p.Estado);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pedidos/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Pedidos == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .Include(p => p.Cliente)
+                .Include(p => p.Estado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Pedidos/Create
+        public IActionResult Create()
+        {
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id");
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id");
+            return View();
+        }
+
+        // POST: Pedidos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Numero,Descripcion,Fecha,TotalVenta,DireccionDeEnvio,EstadoId,ClienteId")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                pedido.Id = Guid.NewGuid();
+                _context.Add(pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", pedido.ClienteId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", pedido.EstadoId);
+            return View(pedido);
+        }
+
+        // GET: Pedidos/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Pedidos == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", pedido.ClienteId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", pedido.EstadoId);
+            return View(pedido);
+        }
+
+        // POST: Pedidos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Numero,Descripcion,Fecha,TotalVenta,DireccionDeEnvio,EstadoId,ClienteId")] Pedido pedido)
+        {
+            if (id != pedido.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ClienteId"] = new SelectList(_context.Clientes, "Id", "Id", pedido.ClienteId);
+            ViewData["EstadoId"] = new SelectList(_context.Estados, "Id", "Id", pedido.EstadoId);
+            return View(pedido);
+        }
+
+        // GET: Pedidos/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Pedidos == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .Include(p => p.Cliente)
+                .Include(p => p.Estado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: Pedidos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Pedidos == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Pedidos'  is null.");
+            }
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido != null)
+            {
+                _context.Pedidos.Remove(pedido);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(Guid id)
+        {
+          return (_context.Pedidos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
